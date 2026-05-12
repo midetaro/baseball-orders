2c2d56b1d1c906d0209a333812bcf71fd1dc536b
QWE123\a3123 <2401.dress@gmail.com>
2026-05-13 00:05:13 +0900

    シミュレーションクラス

---
+++ feature-simulator/lib/src/main/java/org/example/domain/model/GameContext.java
@@ -0,0 +1,73 @@
+package org.example.domain.model;
+
+import org.example.domain.code.BattingResult;
+import org.example.domain.model.player.Batter;
+import org.example.domain.model.state.BasesState;
+import org.example.domain.model.state.NoBasesState;
+
+import java.util.List;
+
+public class GameContext {
+
+    private int inning = 1;
+    private int totalScore = 0;
+    private int outCounts = 0;
+    private BasesState currentBaseState = new NoBasesState(); // 初期状態
+    private final List<Batter> batters;
+    private int numberOfNextBatter;
+
+    public GameContext(List<Batter> batters) {
+        this.batters = batters;
+        this.numberOfNextBatter = 0;
+    }
+
+    public void updateBaseState(BasesState state) {
+        currentBaseState = state;
+    }
+
+    public void addScore(int runs) {
+        totalScore += runs;
+    }
+
+    public void addOut(int diff) {
+        outCounts += diff;
+
+        if (outCounts >= 3) {
+            this.goToNextInning();
+        }
+    }
+
+    private void goToNextInning() {
+        inning++;
+        currentBaseState = new NoBasesState();
+        outCounts = 0;
+        if (inning == 9) {
+            currentBaseState = new NoBasesState();
+        }
+    }
+
+    public boolean isGameOver() {
+        return inning == 9;
+    }
+
+    public void nextAtBat() {
+        var batter = batters.get(numberOfNextBatter);
+        BattingResult battingResult = batter.swing();
+        switch (battingResult) {
+            case OUT -> currentBaseState.out(this, batter);
+            case HIT_SINGLE -> currentBaseState.singleHit(this, batter);
+            case HIT_DOUBLE -> currentBaseState.hitDouble(this, batter);
+            case HIT_TRIPLE -> currentBaseState.hitTriple(this, batter);
+            case HIT_HOMER -> currentBaseState.hitHomer(this, batter);
+        }
+        this.toNextBatter();
+    }
+
+    private void toNextBatter() {
+        if (this.numberOfNextBatter == 8) {
+            this.numberOfNextBatter = 0;
+        }
+        this.numberOfNextBatter++;
+    }
+
+}
