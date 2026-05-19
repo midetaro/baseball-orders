5f6744e9a0ba5788991ad12b6532136baec5eede
QWE123\a3123 <2401.dress@gmail.com>
2026-05-20 00:38:15 +0900

    コメント修正

--- feature-simulator/lib/src/main/java/org/example/domain/model/behavior/LongDistanceBattingBehavior.java
+++ feature-simulator/lib/src/main/java/org/example/domain/model/behavior/LongDistanceBattingBehavior.java
@@ -1,14 +1,26 @@
-//package org.example.domain.behavior;
-//
-//import org.example.domain.code.BattingResult;
-//
-/// **
-// * 長距離バッター
-// */
-//public class LongDistanceBattingBehavior implements AtBatBehavior {
-//
-//    @Override
-//    public BattingResult batting(float hitAverage, float sluggish) {
-//       return BattingResult.HIT_HOMER;
-//    }
-//}
+package org.example.domain.model.behavior;
+
+import org.example.domain.code.BattingResult;
+
+/**
+ * 長距離バッター
+ */
+public class LongDistanceBattingBehavior implements AtBatBehavior {
+
+    @Override
+    public BattingResult batting(float hitAverage, float sluggish) {
+        float random = (float) Math.random();
+        float singleProbability = hitAverage * (1 - sluggish);
+        float doubleProbability = hitAverage * (sluggish / 2);
+        float homerProbability = hitAverage * (sluggish / 2);
+
+        if (random < singleProbability) {
+            return BattingResult.HIT_SINGLE;
+        } else if (random < singleProbability + doubleProbability) {
+            return BattingResult.HIT_DOUBLE;
+        } else if (random < singleProbability + doubleProbability + homerProbability) {
+            return BattingResult.HIT_HOMER;
+        }
+        return BattingResult.OUT;
+    }
+}
