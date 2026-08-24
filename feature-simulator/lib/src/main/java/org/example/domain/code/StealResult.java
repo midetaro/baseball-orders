93245a6037765685f5025150c2227a8aec09b164
midetaro <2401.dress@gmail.com>
2026-08-25 04:31:27 +0900

    remove: removed old feature files

--- feature-simulator/lib/src/main/java/org/example/domain/code/StealResult.java
+++
@@ -1,8 +0,0 @@
-package org.example.domain.code;
-
-public enum StealResult {
-    
-    NOT_TRY,
-    FAILURE,
-    SUCCESS
-}
--- feature-simulator/lib/src/main/java/org/example/domain/model/behavior/AtBatBehavior.java
+++
@@ -1,9 +0,0 @@
-package org.example.domain.model.behavior;
-
-import org.example.domain.code.BattingResult;
-
-public interface AtBatBehavior {
-
-    BattingResult batting(float hitAverage, float sluggish);
-
-}
