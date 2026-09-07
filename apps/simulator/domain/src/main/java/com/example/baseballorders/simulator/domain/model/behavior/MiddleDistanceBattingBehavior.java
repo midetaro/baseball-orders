eccb0c6759066d56990d82df2352c3fd5152779e
midetaro <2401.dress@gmail.com>
2026-09-07 23:47:07 +0900

    feat: simulator_打率のロジックを長打に寄せる

--- apps/simulator/domain/src/main/java/com/example/baseballorders/simulator/domain/model/behavior/MiddleDistanceBattingBehavior.java
+++ apps/simulator/domain/src/main/java/com/example/baseballorders/simulator/domain/model/behavior/MiddleDistanceBattingBehavior.java
@@ -1,14 +1,48 @@
 package com.example.baseballorders.simulator.domain.model.behavior;
 
 import com.example.baseballorders.simulator.domain.code.BattingResult;
+import com.example.baseballorders.simulator.domain.util.RandomGenerator;
 import org.springframework.stereotype.Component;
 
 /** 中距離バッター */
 @Component("middleDistanceAtBat")
 public class MiddleDistanceBattingBehavior implements AtBatBehavior {
 
-    @Override
-    public BattingResult batting(float hitAverage, float sluggish) {
-        return BattingResult.HIT_DOUBLE;
+    public BattingResult batting(float hitAverage, float slugging) {
+        float random = RandomGenerator.nextFloat();
+
+        // 長打によって増えた塁数
+        float extraBaseProbability = slugging - hitAverage;
+
+        // 二塁打・三塁打・本塁打を同じ確率と仮定
+        float doubleProbability = extraBaseProbability / 6;
+        float tripleProbability = extraBaseProbability / 6;
+        float homeRunProbability = extraBaseProbability / 6;
+
+        float singleProbability =
+                hitAverage - extraBaseProbability / 2;
+
+        float cumulative = singleProbability;
+
+        if (random < cumulative) {
+            return BattingResult.HIT_SINGLE;
+        }
+
+        cumulative += doubleProbability;
+        if (random < cumulative) {
+            return BattingResult.HIT_DOUBLE;
+        }
+
+        cumulative += tripleProbability;
+        if (random < cumulative) {
+            return BattingResult.HIT_TRIPLE;
+        }
+
+        cumulative += homeRunProbability;
+        if (random < cumulative) {
+            return BattingResult.HIT_HOMER;
+        }
+
+        return BattingResult.OUT;
     }
 }
